ed7c55c2d677553840fa39588551c83fd4eb48c4
Kenneth Moy <moykenneth91@gmail.com>
2016-09-19 14:14:03 -0400

    Added functions up to curl

--- bash.js
+++ bash.js
@@ -5,12 +5,15 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-	var cmd = data.toString().trim(); // remove the newline
+    //var cmd = data.toString().trim(); // remove the newline
+    var words = data.toString().trim().split(" ");
+    var cmd = words[0];
+    var args = words.slice(1).join(' ');
 
-	if (commands.hasOwnProperty(cmd)) {
-		commands[cmd]();
-	} else {
-		process.stdout.write('You typed: ' + cmd);
-	}
-
+    if (commands.hasOwnProperty(cmd)) {
+        commands[cmd](args);
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write("\nprompt > ");
+    }
 });
--- commands.js
+++ commands.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
+var request = require('request');
 
-module.exports = { 
+module.exports = {
 	pwd: function() {
-		process.stdout.write(process.cwd())
+		process.stdout.write(process.cwd());
 		process.stdout.write("\nprompt > ");
 	},
 	date: function() {
@@ -15,5 +16,59 @@ module.exports = { 
 			process.stdout.write(file.toString() + "\n");
 		});
 		process.stdout.write("prompt > ");
-	});}
+	});},
+	echo: function(words){
+		process.stdout.write(words + '\n');
+		process.stdout.write("prompt > ");		
+	},
+	cat: function(filenames){
+		var files = filenames.split(' ');
+		files.forEach(function(file){
+			process.stdout.write(fs.readFileSync(file, "utf-8") + '\n');
+		})
+		process.stdout.write("prompt > ");		
+	},
+	head: function(filenames){
+		var files = filenames.split(' ');
+		files.forEach(function(file){
+			var lines = fs.readFileSync(file, "utf-8").split('\n');
+			for(var i = 0; i < 5; ++i){
+				process.stdout.write(lines[i] + '\n');
+			}
+		});
+		process.stdout.write("prompt > ");		
+	},
+	tail: function(filenames){
+		var files = filenames.split(' ');
+		files.forEach(function(file){
+			var lines = fs.readFileSync(file, "utf-8").split('\n');
+			for(var i = lines.length - 5; i < lines.length; ++i){
+				process.stdout.write(lines[i] + '\n');
+			}
+		});
+		process.stdout.write("prompt > ");		
+	},
+	wc: function(filenames){
+		var files = filenames.split(' ');
+		files.forEach(function(file){
+			var lines = fs.readFileSync(file, "utf-8").split('\n');
+			process.stdout.write((lines.length - 1).toString() + '\n');
+		});
+		process.stdout.write("prompt > ");		
+	},
+	sort: function(filenames){
+		var files = filenames.split(' ');
+		files.forEach(function(file){
+			var lines = fs.readFileSync(file, "utf-8").split('\n');
+			process.stdout.write(lines.sort().join('\n'));
+		});
+		process.stdout.write("prompt > ");		
+	},
+	curl: function(link) {
+		request(link, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+			}
+		})
+	},
 };
